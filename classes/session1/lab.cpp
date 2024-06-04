1fced71608ada2281a8dd062c7a14cc85465206b
ornelas15 <david.ornelas@ua.pt>
2024-06-04 19:01:22 +0100

    update lab1

--- classes/session1/lab.cpp
+++ classes/session1/lab.cpp
@@ -6,9 +6,74 @@ David Ornelas
 #include <iostream>	
 using namespace std;
 
+int g; // global variable
+
 int main(){
-	cout << "Hello, World!\n";
+	
+	/* Class examples:
+	
+	// variable declaration
+	int number = 0;
+	int n1, n2, n;
+	char key;
+	// variable initialization
+	number = 5;
+	n1 = 10;
+	n2 = 20;
+	// variable assignment
+	g = number + 1;
+	n = n1 + n2;
+
+	// << operator sends to cout the text and variables 
+	with a new line (endl) at the end of the line: 
+	cout << "\nHello, World!" << endl;
+	cout << g << endl;
+	cout << n << endl;
+
+	// Wait for user input
+	cout << "Press Key: ";
+	cin >> key; // read a character from the keyboard
+	cout << "Key pressed: " << key << endl; // display key pressed
+	
 	return 0;
+	*/
+	
+	
+	// Lab 1 - exercises:
+
+	
+	/* Exercise 1 
+	int a = 1990;
+	int b = 135;
+	int c = 7290;
+	int d = 11300;
+	int e = 16200;
+	int f, g, h;
+
+	f = a+1;
+	g = a+2;
+	h = a+3;
+
+	cout << " " << a << " " << b << endl 
+		<< " " << f << " " << c << endl
+		<< " " << g << " " << d << endl
+		<< " " << h << " " << e << endl;
+	
+	return 0;
+	*/
+
+	
+	/* Exercise 2
+	int a = 10;
+	int b = 20;
+
+	cout << "\n" << a << "\n" << endl;
+	cout << b << "\n" << endl;
+	b--;
+	cout << b << endl;
+	return 0;
+	*/
+	
 }
 
 
