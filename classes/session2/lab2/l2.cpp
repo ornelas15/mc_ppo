6ee8b21e517436d1c1dfdfd3288e3f5497303283
ornelas15 <david.ornelas@ua.pt>
2024-06-13 15:13:46 +0100

    add l2 of s3

---
+++ classes/session2/lab2/l2.cpp
@@ -0,0 +1,32 @@
+/*  
+Lab 2 - 06-June-2024
+David Ornelas
+
+Ex.2
+Implement a program to count words and count the occurrence of
+a specific keyword. You should only consider exact matches, i.e.
+if the keyword is "contained" inside another word should not be 
+counted. The program should start getting from the user a keyword 
+and then gets paragraphs (a paragraph finishes with the symbol '$')
+in an iterative way (the user has to write a paragraph with the 
+word "End" to finish the program). For each introduced paragraph,
+the program should count the number of words in the paragraph 
+(consider the most common symbols as separators) and count
+the number of occurrences of the keyword.
+*/
+
+#include <iostream>	
+#include <iomanip>
+using namespace std;
+
+
+
+int main(){
+
+
+
+	return 0;
+}
+
+
+
