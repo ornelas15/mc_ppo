0c506e6300bad6f32a5bd973bd76b4e47dec253e
ornelas15 <david.ornelas@ua.pt>
2024-06-18 17:01:07 +0100

    update l9 e l10

--- classes/session3/lab3/l10.cpp
+++ classes/session3/lab3/l10.cpp
@@ -18,8 +18,36 @@ Provide two different implementations.
 #include <bitset>
 using namespace std;
 
+// Function to calculate the nth Fibonacci number using recursion
+int fib(int n) {
+	if (n <= 1) {return n;} 
+	return fib(n - 1) + fib(n - 2);
+}
+
+// Second function to calculate the nth Fibonacci number using iteration
+int fib2(int n) { 
+    // Declare an array to store Fibonacci numbers. 
+    int f[n + 2];  
+    int i; 
+
+    // 1st numbers of the series are 0 and 1 
+    f[0] = 0; 
+    f[1] = 1; 
+  
+    for(i = 2; i <= n; i++) { 
+       //Add the previous 2 numbers in the series and store it 
+       f[i] = f[i - 1] + f[i - 2]; 
+    } 
+    return f[n]; 
+} 
+  
 
 int main(){
+
+	// Fibonacci sequence
+	int n = 40;
+	cout << "Fibonacci(" << n << "): " << fib(n) << endl;
+	cout << "Fibonacci(" << n << "): " << fib2(n) << endl;
 
 	return 0;
 }
--- classes/session3/lab3/l9.cpp
+++ classes/session3/lab3/l9.cpp
@@ -6,23 +6,34 @@ Ex.9:
 Write a function which calculates the greatest common divisor 
 between two integer positive numbers using the Euclidean algorithm, 
 given by the following definition:
-gdc(a,b) = a if b = 0; gdc(r,b) if b > 0.
-where r is the remainder of the division of a by b. 
+gdc(a,b) = a if b = 0; gdc(r,b) if b > 0, where r is the remainder of the division of a by b. 
 Use it in a program which requests that value to the user. 
 Provide two different implementations.
 
 */
 
-#include <iostream>	
-#include <bitset>
+#include <iostream>
 using namespace std;
 
-
-
-int main(){
-
-	return 0;
+// Function to calculate the greatest common divisor between two numbers using recursion
+int gcd(int a, int b) {
+    if (b == 0) {
+        return a;
+    } else {
+		int r = a % b;
+        return gcd(b, r);} 
 }
 
+int main() {
+    int a, b;
+    cout << "Enter two positive numbers: ";
+    cin >> a >> b;
+    if (a > 0 && b > 0) {
+        cout << "Greatest common divisor: " << gcd(a, b) << endl;
+    } else {
+        cout << "Please enter only positive numbers." << endl;
+    }
+    return 0;
+}
 
 
